dcacb9f833d2cf73dce2a4266279150fdda3a7e0
Bhawna615 <bansalbhanwna@gmail.com>
2025-09-17 14:05:02 +0530

    Update TopbarStart.jsx

--- my-app/src/project/TopbarStart.jsx
+++ my-app/src/project/TopbarStart.jsx
@@ -3,38 +3,40 @@ import "./TopBar.css"; // import css
 
 function TopBar() {
   return (
-    <div className="topbar bg-dark">
-      <div className="topbar-left">
-        <small>
-          <i className="fa fa-phone-alt mr-2"></i>
-          98171-96968
-        </small>
-        <span className="divider">|</span>
-        <a
-          href="mailto:blossomsactivity.sol@gmail.com"
-          className="email-link"
-        >
-          <i className="fa fa-envelope mr-2"></i>
-          blossomsactivity.sol@gmail.com
-        </a>
-      </div>
+    <div className="topbar bg-dark ">
+      <div class="topbar-container" style={{display:"flex",justifyContent:"space-between",alignItems:"center",padding:"5px 40px",width:"100%"}}>
+        <div className="topbar-left">
+          <small>
+            <i className="fa fa-phone-alt mr-2"></i>
+            98171-96968
+          </small>
+          <span className="divider">|</span>
+          <a
+            href="mailto:blossomsactivity.sol@gmail.com"
+            className="email-link"
+          >
+            <i className="fa fa-envelope mr-2"></i>
+            blossomsactivity.sol@gmail.com
+          </a>
+        </div>
 
-      <div className="topbar-right">
-        <a className="social" href="https://www.facebook.com/KKBHSS/">
-          <i className="fab fa-facebook-f"></i>
-        </a>
-        <a className="social" href="https://kkblossomschool.org/#">
-          <i className="fab fa-twitter"></i>
-        </a>
-        <a
-          className="social"
-          href="https://www.linkedin.com/in/samiksha-chaudhary-96a531159?originalSubdomain=in"
-        >
-          <i className="fab fa-linkedin-in"></i>
-        </a>
-        <a className="social" href="https://kkblossomschool.org/#">
-          <i className="fab fa-instagram"></i>
-        </a>
+        <div className="topbar-right">
+          <a className="social" href="https://www.facebook.com/KKBHSS/">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a className="social" href="https://kkblossomschool.org/#">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a
+            className="social"
+            href="https://www.linkedin.com/in/samiksha-chaudhary-96a531159?originalSubdomain=in"
+          >
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+          <a className="social" href="https://kkblossomschool.org/#">
+            <i className="fab fa-instagram"></i>
+          </a>
+        </div>
       </div>
     </div>
   );
